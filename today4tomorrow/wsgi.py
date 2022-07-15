9022829b1f7a0d1a113da80baf47d9041f79a516
Duy Trinh <trinhphuduy102@gmail.com>
2022-07-15 16:14:54 +0700

    First commit

---
+++ today4tomorrow/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for today4tomorrow project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'today4tomorrow.settings')
+
+application = get_wsgi_application()
